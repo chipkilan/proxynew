19fc722485ea51913e70d181e3eafc3e024a95f3
chipkilan <121478302+chipkilan@users.noreply.github.com>
2022-12-27 13:52:32 +0700

    Update 8.sh

--- scripts/8.sh
+++ scripts/8.sh
@@ -54,9 +54,6 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-auth strong
-
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
 $(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
